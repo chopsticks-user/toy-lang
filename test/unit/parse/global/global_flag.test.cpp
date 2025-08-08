ddd5d0d18e6dfda407bbc84a4ac2258738ac494c
chopsticks-user <frostyfrost273@gmail.com>
2025-08-08 14:02:47 -0400

    [Parse] Test cases for each global entity

---
+++ test/unit/parse/global/global_flag.test.cpp
@@ -0,0 +1,22 @@
+#include "parse.test.hpp"
+
+TEST_CASE_WITH_FIXTURE(
+    "Parse.Global.Flag: Empty",
+    "[Unit][Parse][Global]"
+) {
+    // assertTranslationUnit({
+    //     .source =
+    //     "for e in r0 {}",
+    //
+    //     .expectedAstPrint =
+    //     "stmt::Loop [@0:0]\n"
+    //     "├─ decl::Identifier [@0:4] with name = 'e'\n"
+    //     "   ├─ (empty)\n"
+    //     "├─ expr::Identifier [@0:9] with path = 'r0'\n"
+    //     "├─ stmt::Block [@0:12] with size = 0",
+    //
+    //     .expectedPrettyPrint =
+    //     "for e: {?} in r0 {\n"
+    //     "}",
+    // });
+}
